de330ab1a9e3a2c743b2693059afdbaf3e2090ef
juliancochran <julian.p.cochran@gmail.com>
2022-11-14 10:51:32 -0500

    Main class of BMI Calculator lab.

--- src/BMICalculator.java
+++ src/BMICalculator.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class BMICalculator {
+public class BMICalculator {
+    /** Convert English to metric units, perform the BMI calculation.
+     * NOTE: this method must properly handle bad data
+     * @param inches user's height in inches
+     * @param pounds user's weight in pounds
+     * @return User's BMI weight(kg)/height(m)^2 a double
+     */
+    public static double computeBMI(int inches, int pounds) {
+        return 0.0;
+    }
 }
